8c3d1985987e3af2c620d8fa992ea07c954c7e7a
Michael Orji <princemikeorji@gmail.com>
2022-01-29 08:46:24 +0100

    readme write-up & form update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # invoice-app
+
+## Description
+This app can generate, send and recieve invoices, as well as send automated follow-up reminders about overdue payments.
--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,23 @@
-from django.forms import ModelForm
+from turtle import textinput
+from django import forms
+from django.forms import TextInput, ModelForm
 from .models import Bill, Client
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = '__all__'
+        widgets = {'phone_number': TextInput(attrs={
+            'placeholder': '+234'
+            })}
 
 class BillForm(ModelForm):
     class Meta:
         model = Bill
         fields = '__all__'
         exclude = ('item',)
+        widgets = {'pay_date': DateInput}
+
