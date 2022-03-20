6f82d2e9dc0c2512c9a0d5805c14072150424bb0
Michael Orji <princemikeorji@gmail.com>
2022-03-20 02:15:58 +0100

    refactored models

--- core/admin.py
+++ core/admin.py
@@ -3,5 +3,11 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Invoice)
-admin.site.register(Service)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'uuid')
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'quantity', 'rate', 'due_date')
+
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(Service, ServiceAdmin)
--- core/models.py
+++ core/models.py
@@ -1,39 +1,50 @@
 from django.db import models
-from shortuuidfield import ShortUUIDField
+from django.db.models import F, Sum
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+
+from decimal import Decimal
+from shortuuidfield import ShortUUIDField
 
 # Create your models here.
 
 class Invoice(models.Model):
-    uuid = ShortUUIDField(unique=True)
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=15)
-    email = models.EmailField()
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    organisation = models.CharField(max_length=50, blank=True)
-    date = models.DateField()
-
-    def __str__(self):
-        return f"{self.last_name}, {self.first_name}"
-
-class Service(models.Model):
     PAYMENT_STATUS = (
         (1, 'Pending'),
         (2, 'Paid')
     )
-    customer = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    uuid = ShortUUIDField(unique=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    address_one = models.CharField(max_length=100)
+    address_two = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    created_on =  models.DateField(auto_now_add=True)
+    status = models.PositiveIntegerField(choices=PAYMENT_STATUS)
+
+    def __str__(self):
+        return self.name
+
+class Service(models.Model):
     service = models.CharField(max_length=25)
-    description = models.TextField(blank=True)
     quantity = models.IntegerField(default=1)
     rate = models.DecimalField(max_digits=9, decimal_places=2)
-    total_amount = models.DecimalField(max_digits=9, decimal_places=2, blank=True, null=True)
     due_date = models.DateField()
-    status = models.PositiveIntegerField(choices=PAYMENT_STATUS, default=1)
 
     @property
     def rate_display(self):
         return "$%s" % self.rate
 
+    @property
+    def total_price(self):
+        return self.service_set.aggregate(
+            total_price=Sum(F('quantity') * F('rate'))
+        )['total_price'] or Decimal('0')
+
     def __str__(self):
-        return f"{self.service}"
+        return self.service
+
